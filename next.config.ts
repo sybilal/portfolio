77db40a4580a796fc93ae5484ae1accd79ee5424
Bilal Syed <sbilalahmed95@gmail.com>
2026-04-16 20:54:30 +0200

    repo name fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production'
 const repo = 'portfolio'
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
